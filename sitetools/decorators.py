4f5bb6afa30b3daa9a758f07729b2872b1179881
R3v1L <ogutsua@gmail.com>
2015-06-28 10:39:25 +0100

    Fixed missing import

--- sitetools/decorators.py
+++ sitetools/decorators.py
@@ -5,7 +5,7 @@
 
 .. module:: sitetools.decorators
     :platform: Unix, Windows
-    :synopsis: 
+    :synopsis:
     :deprecated:
 .. moduleauthor:: (C) 2014 Oliver Gutiérrez
 """
@@ -14,17 +14,18 @@
 from functools import wraps
 
 # Django imports
-from django.http import HttpResponse,HttpResponseRedirect,Http404
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden, Http404
 from django.utils.decorators import available_attrs
 
 # Application imports
 from sitetools.utils import get_client_ip
 
+
 def ajax_or_redirect(redirect_url='/'):
     """
     Decorator for views that checks that the request is an AJAX request, redirecting
     to the specified URL if it is not.
-    
+
     :param redirect_url: Value to be checked
     :type redirect_url: An absolute or relative URL
     """
@@ -37,13 +38,14 @@ def ajax_or_redirect(redirect_url='/'):
         return _wrapped_view
     return decorator
 
-def check_ip(ip_list,errorcode=403,redirect_url=None):
+
+def check_ip(ip_list, errorcode=403, redirect_url=None):
     """
     Decorator to check client IP is allowed returning an error or redirecting to
     the specified URL in case of it is not.
 
     :param ip_list: List of allowed IP addresses
-    :type ip_list: list    
+    :type ip_list: list
     :param statuscode: Status code for denied request
     :type statuscode: int
     :param redirect_url: Value to be checked
@@ -52,15 +54,16 @@ def check_ip(ip_list,errorcode=403,redirect_url=None):
     def decorator(view_func):
         @wraps(view_func, assigned=available_attrs(view_func))
         def _wrapped_view(request, *args, **kwargs):
-            ip=get_client_ip(request)
+            ip = get_client_ip(request)
             if ip not in ip_list:
                 if redirect_url is not None:
-                    return HttpResponseRedirect(redirect_url) 
+                    return HttpResponseRedirect(redirect_url)
                 else:
                     return HttpResponse(status=errorcode)
             return view_func(request, *args, **kwargs)
         return _wrapped_view
     return decorator
+
 
 def ajax_or_404(view_func):
     """
@@ -72,6 +75,7 @@ def ajax_or_404(view_func):
             return view_func(request, *args, **kwargs)
         raise Http404
     return _wrapped_view
+
 
 def logged_in_or_403(view_func):
     """
