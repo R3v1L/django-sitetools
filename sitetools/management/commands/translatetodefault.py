be9d747b8ca4c5d18f9725b2dad08dba6119d810
Oliver Gutierrez <ogutierrez@redhat.com>
2017-04-03 09:44:39 +0100

    Fixed translatetodefault in new django version

--- sitetools/management/commands/translatetodefault.py
+++ sitetools/management/commands/translatetodefault.py
@@ -21,40 +21,43 @@ from django.conf import settings
 
 
 class Command(BaseCommand):
-	args = '<lang lang ...>'
-	help = _('Set translations to default translation string')
+    args = '<lang lang ...>'
+    help = _('Set translations to default translation string')
 
-	option_list = BaseCommand.option_list + (
-        make_option('--fullreplace',
+    def add_arguments(self, parser):
+        # Positional arguments
+        parser.add_argument('lang', nargs='+', type=str)
+
+        # Named (optional) arguments
+        parser.add_argument(
+            '--fullreplace',
             action='store_true',
             dest='fullreplace',
             default=False,
             help=_('Replace existing translations too')
-        ),
-        make_option('--savemo',
+        )
+        parser.add_argument(
+            '--savemo',
             action='store_true',
             dest='savemo',
             default=False,
-            help=_('Replace existing translations too')
-        ),
-    )
+            help=_('Save .mo file')
+        )
 
-	def handle(self, *args, **options):
-		"""
-		Command handling
-		"""
-		if not args:
-			raise CommandError(_('You must specify at least one language to process'))
-		for lang in args:
-			filename='locale/%s/LC_MESSAGES/django.po' % lang
-			try:	
-				po = polib.pofile(filename)
-				for entry in po:
-					if options['fullreplace'] or not entry.msgstr:
-						entry.msgstr=entry.msgid
-				po.save()
-				if options['savemo']:
-					po.save_as_mofile('locale/%s/LC_MESSAGES/django.mo' % lang)
-			except Exception, e:
-				self.stdout.write(_('ERROR: Can not open %(filename)s: %(error)s\n') % (filename,e))
-				# raise CommandError(_('Unexpected error raising anunciosx.com advertisements: %s') % e)
+    def handle(self, *args, **options):
+        """
+        Command handling
+        """
+        for lang in options['lang']:
+            filename = 'locale/%s/LC_MESSAGES/django.po' % lang
+            try:
+                po = polib.pofile(filename)
+                for entry in po:
+                    if options['fullreplace'] or not entry.msgstr:
+                        entry.msgstr = entry.msgid
+                po.save()
+                if options['savemo']:
+                    po.save_as_mofile('locale/%s/LC_MESSAGES/django.mo' % lang)
+            except Exception, e:
+                self.stdout.write(
+                    _('ERROR: Can not open %s: %s\n') % (filename, e))
